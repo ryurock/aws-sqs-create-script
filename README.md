d308df80e1c5af43c3fe646c9ddf993bede5a793
ryurock <ryusukekimura3@gmail.com>
2016-12-14 23:50:17 +0900

    initialize commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # aws-sqs-create-script
+
 AWS CLI Create SQS sample
+
+## Requirement
+
+* [aws-cli](https://aws.amazon.com/jp/cli/)
+
+## Usage
+
+```
+sh create-sqs.sh {{ queue name }}
+```
+
---
+++ create_sqs.sh
@@ -0,0 +1,45 @@
+#!/bin/env/bash
+
+SQS_QUEUE_NAME=$1
+echo "Try Create SQS queue name: ${SQS_QUEUE_NAME}"
+
+QUEUE_NAME_EXISTS=$(aws sqs list-queues --queue-name-prefix ${SQS_QUEUE_NAME})
+
+if [ ${#QUEUE_NAME_EXISTS} -gt 0 ]; then
+    echo "InValid Que name. not uniq Que name"
+    exit 1
+fi
+
+aws sqs create-queue --queue-name ${SQS_QUEUE_NAME}
+echo "Created SQS QUEUE name: ${SQS_QUEUE_NAME}"
+aws sqs list-queues --queue-name-prefix ${SQS_QUEUE_NAME}
+# SQS VisibilityTimeout (VisibilityTimeoutは、受信操作を行うとそのメッセージが見えなくなる一定の時間のことです。 Timeout前にそのメッセージに対して削除操作がされないと、再度キューに戻されます。)
+SQS_VTOUT=60
+
+echo "Modified SQS queue-attributes VisibilityTimeout: ${SQS_VTOUT}"
+SQS_QUEUE_URL=$( \
+        aws sqs get-queue-url \
+          --queue-name ${SQS_QUEUE_NAME} \
+          --output text \
+)
+aws sqs set-queue-attributes --queue-url ${SQS_QUEUE_URL} --attributes VisibilityTimeout=${SQS_VTOUT}
+
+echo "Setting SQS QUEUE: ${SQS_QUEUE_NAME}"
+aws sqs get-queue-attributes \
+        --queue-url ${SQS_QUEUE_URL} \
+        --attribute-names \
+          Policy \
+          VisibilityTimeout \
+          MaximumMessageSize \
+          MessageRetentionPeriod \
+          ApproximateNumberOfMessages \
+          ApproximateNumberOfMessagesNotVisible \
+          CreatedTimestamp \
+          LastModifiedTimestamp \
+          QueueArn \
+          ApproximateNumberOfMessagesDelayed \
+          DelaySeconds \
+          ReceiveMessageWaitTimeSeconds \
+          RedrivePolicy
+
+echo "SQS Setting Finish URL: ${SQS_QUEUE_URL}"
